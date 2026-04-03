8231193edf401835a3504b9df8641b7fa5e91134
Rodrigo <rodrigocardosotadeo@gmail.com>
2026-04-02 23:23:08 -0300

    melhorando a ux do projeto

--- src/FiapBankAtm.java
+++ src/FiapBankAtm.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class FiapBankAtm {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -9,7 +8,7 @@ public class FiapBankAtm {
         int indiceEspaco;
         String primeiroNomeUsuario;
 
-        System.out.println("\n=== CADASTRO DE USUÁRIO ===");
+        System.out.println("\n=== FIAP BANK ATM - CADASTRO DE USUÁRIO ===");
 
         do {
             System.out.println("Informe o Nome Completo (nome e sobrenome): ");
@@ -30,7 +29,7 @@ public class FiapBankAtm {
         String senhaForte;
 
         do {
-            System.out.println("Informe um senha forte: ");
+            System.out.println("\nInforme um senha forte: ");
             senhaForte = input.nextLine();
 
             if (!senhaForte.matches(regex)) {
@@ -42,7 +41,7 @@ public class FiapBankAtm {
         System.out.println("Senha cadastrada com sucesso para o usuário " + nomeUsuario.toUpperCase());
         System.out.println("Cadastro finalizado!");
 
-        System.out.println("\n=== LOGIN DE USUÁRIO ===");
+        System.out.println("\n=== FIAP BANK ATM - LOGIN DE USUÁRIO ===");
 
         int contadorTentativas = 0;
         String senhaLoggin;
@@ -69,57 +68,79 @@ public class FiapBankAtm {
 
 
         String menuPrincipal = """
-               === MENU PRINCIPAL ===
+               \n=== FIAP BANK ATM - MENU PRINCIPAL ===
                [1] Consultar Saldo
                [2] Fazer Depósito
                [3] Fazer Saque
                [4] Sair
+               ======================================
                """;
 
         double saldo = 0.0;
+        boolean sistemaAtivo = true;
 
-        Menu:
-        while (true) {
+        while (sistemaAtivo) {
             System.out.println(menuPrincipal);
             System.out.println("Selecione uma opção: ");
-            int opcaoMenu = input.nextInt();
-            input.nextLine();
-            switch (opcaoMenu) {
-                case 1:
-                    System.out.printf("Saldo Atual: R$%.2f%n", saldo);
-                    break;
-                case 2:
-                    System.out.println("Informe o valor do depósito: ");
-                    double deposito = input.nextDouble();
-                    input.nextLine();
-                    if (deposito <= 0) {
-                        System.out.println("Valor de depósito inválido");
-                        break;
-                    } else {
-                        saldo += deposito;
-                        System.out.printf("R$%.2f Depositado com sucesso%n", deposito);
-                        break;
-                    }
-                case 3:
-                    System.out.println("Informe o valor do saque: ");
-                    double saque = input.nextDouble();
-                    input.nextLine();
-                    if (saque <= 0 || saque > saldo) {
-                        System.out.println("Valor de saque inválido");
-                        break;
-                    } else {
-                        saldo -= saque;
-                        System.out.printf("R$%.2f Sacado com sucesso%n", saque);
-                        break;
-                    }
-                case 4:
-                    System.out.println("O FIAP Bank agradece sua preferência!");
-                    break Menu;
-                default:
-                    System.out.println("Opção inválida. Escolha uma opção válida");
 
+            if (input.hasNextInt()) {
+                int opcaoMenu = input.nextInt();
+                input.nextLine();
+
+                switch (opcaoMenu) {
+                    case 1:
+                        System.out.printf("=== Saldo Atual: R$%.2f ===%n", saldo);
+                        break;
+
+                    case 2:
+                        System.out.println("Informe o valor do depósito: R$");
+                        if (input.hasNextDouble()) {
+                            double deposito = input.nextDouble();
+                            input.nextLine();
+                            if (deposito <= 0) {
+                                System.out.println("Erro: Valor de depósito inválido. O valor deve ser maior que zero.");
+                            } else {
+                                saldo += deposito;
+                                System.out.printf("=== R$%.2f Depositado com sucesso ===%n", deposito);
+                            }
+                        } else {
+                            System.out.println("Erro: Por favor, digite apenas números.");
+                            input.nextLine();
+                        } break;
+
+                    case 3:
+                        System.out.println("Informe o valor do saque: ");
+                        if (input.hasNextDouble()) {
+                            double saque = input.nextDouble();
+                            input.nextLine();
+                            if (saque <= 0) {
+                                System.out.println("Erro: Valor de saque inválido");
+                            } else if (saque > saldo) {
+                                System.out.println("Erro: Saldo insuficiente para esta operação");
+                            } else {
+                                saldo -= saque;
+                                System.out.printf("=== R$%.2f Sacado com sucesso ===%n", saque);
+                            }
+                        } else {
+                            System.out.println("Erro: Por favor, digite apenas números.");
+                            input.nextLine();
+                        } break;
+
+                    case 4:
+                        System.out.println("O FIAP Bank agradece sua preferência!");
+                        sistemaAtivo = false;
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida. Escolha uma opção válida");
+                        break;
+
+                }
+            } else {
+                System.out.println("Erro: Por favor, digite apenas números.");
+                input.nextLine();
             }
         }
-
+        input.close();
     }
 }
